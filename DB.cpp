14d2fcbf5a442139945e7ab93a52ece1c92d084e
Tomasz <nemaider19@gmail.com>
2020-01-18 01:16:26 +0100

    before shutdown

--- DB.cpp
+++ DB.cpp
@@ -120,14 +120,14 @@ void DB::get_event(String user,String year,String month,String day,TLabel *label
         mysql_close(connect);
 }
 
-void DB::get_event_id(String year,String month,String day,TLabel *label)
+void DB::get_event_id(String user,String year,String month,String day,TLabel *label)
 {
         MYSQL *connect=connect_db();
         if(connect==NULL)
                return;
 
         AnsiString caption;
-        String zapytanie="SELECT id_zadania FROM eventy WHERE data='"+year+"-"+month+"-"+day+"'"+" ORDER BY godzina ASC";
+        String zapytanie="SELECT id_zadania FROM eventy WHERE user='"+user+"' AND data='"+year+"-"+month+"-"+day+"'"+" ORDER BY godzina ASC";
         char *query=zapytanie.c_str();
 
         int qstate=mysql_query(connect,query);
@@ -137,7 +137,7 @@ void DB::get_event_id(String year,String month,String day,TLabel *label)
                 MYSQL_ROW row=mysql_fetch_row(res);
                 while(row)
                 {
-                        caption=caption+"\n"+row[0]+" "+row[1];
+                        caption=caption+"\n"+row[0];
                         row=mysql_fetch_row(res);
                 }
         }
@@ -172,20 +172,60 @@ void DB::add_event(String user,String nazwa,String year,String month,String day,
                 ShowMessage("Nie udalo siê dodac wydarzenia!");
 }
 
-void DB::delete_event(String id)
+void DB::delete_event(String user,String id,String year,String month,String day)
 {
         MYSQL *connect=connect_db();
         if(connect==NULL)
                 return;
 
-        AnsiString caption;
+        int status=0;
         String usuwanie;
+        // walidacja id
 
-        usuwanie="DELETE FROM `eventy` WHERE `eventy`.`Id_zadania` = '"+id+"'";
-
-        char *query=usuwanie.c_str();
+        String caption;
+        String zapytanie="SELECT id_zadania FROM eventy WHERE user='"+user+"' AND data='"+year+"-"+month+"-"+day+"'"+" ORDER BY godzina ASC";
+        char *query=zapytanie.c_str();
 
         int qstate=mysql_query(connect,query);
-        if(qstate!=0)
-                ShowMessage("Nie udalo sie usunac wydarzenia!");
+        if(qstate==0)
+        {
+                MYSQL_RES *res = mysql_use_result (connect);
+                MYSQL_ROW row=mysql_fetch_row(res);
+                while(row)
+                {
+                        caption=row[0];
+                        row=mysql_fetch_row(res);
+                        if(id==caption)
+                        {
+                                usuwanie="DELETE FROM `eventy` WHERE user='"+user+"' AND `eventy`.`Id_zadania` = '"+id+"'";
+
+                                char *query1=usuwanie.c_str();
+                                int qstate1=mysql_query(connect,query1);
+
+                                if(qstate1!=0){
+
+                                        ShowMessage("Nie udalo sie usunac wydarzenia!");
+
+                                } else {
+                                        status=1;
+                                        ShowMessage("Usunieto zadanie");
+                                }
+                        }
+
+                }
+                if(status==0)
+                        ShowMessage("Podano zly numer zadania!");
+        }
+
+        // koniec walidacji
+
+
+
+        //usuwanie="DELETE FROM `eventy` WHERE user='"+user+"' AND `eventy`.`Id_zadania` = '"+id+"'";
+
+        //char *query1=usuwanie.c_str();
+
+        //int qstate1=mysql_query(connect,query1);
+       // if(qstate1!=0)
+               // ShowMessage("Nie udalo sie usunac wydarzenia!");
 }
